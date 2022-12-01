2b176db5540d82e963a4794f3ea8e48131386139
Jest13 <camil.hirane@ynov.com>
2022-12-01 16:22:21 +0100

    checkbox form

--- vitrine/components/HeaderComponent.jsx
+++ vitrine/components/HeaderComponent.jsx
@@ -1,12 +1,17 @@
 import Head from "next/head";
 import React from "react"
+import style from "./Footer/style";
 
 const HeadComponent = () => {
     return (
-        <Head>
-            <title>Ride</title>
-            <meta name="description" content="" />
-        </Head>
+        <div>
+            <input type="checkbox" id="entreprise" name="entreprise" value="une entreprise"/>
+            <label id={"entreprise"}>une entreprise </label>
+            <input type="checkbox" id="particulier" name="entreprise" value="un particulier"/>
+            <label id={"particulier"}>un particulier </label>
+        </div>
+
+
     );
 };
 
