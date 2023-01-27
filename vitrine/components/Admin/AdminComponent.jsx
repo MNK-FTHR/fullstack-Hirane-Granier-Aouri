970507f98d21c0ebb07420633136aa081b4cc8a9
MNK-FTHR <victorgranier.pierre@gmail.com>
2023-01-27 16:59:46 +0100

    d

--- vitrine/components/Admin/AdminComponent.jsx
+++ vitrine/components/Admin/AdminComponent.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import style from "./style.js";
 const AdminComponent = (props) => {
+
     return(
-        <div>
-            admin
+        <div style={style.container}>
+            <div style={style.nav}>
+                <div style={style.nav1}>
+                    <p></p>
+                </div>
+                <div style={style.nav2}>
+
+                </div>
+            </div>
+            <div style={style.table}></div>
         </div>
     )
 }
--- vitrine/components/Admin/style.js
+++ vitrine/components/Admin/style.js
@@ -6,8 +6,21 @@ const style = {
         gap: "0px 0px",
         gridTemplateAreas: `
             "nav"
-            "."
+            "table"
         `
-    }
+    },
+    nav: {
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateRows: "1fr",
+        gap: "0px 0px",
+        gridTemplateAreas: `
+            "nav1 nav2"
+        `,
+        gridArea: "nav",
+    },
+    nav1: { gridArea: "nav1"},
+    nav2: { gridArea: "nav2"},
+    table: { gridArea: "table" }
 }
 export default style;
