750dc819c408397f373791622a8f0c2bc03a1365
MNK-FTHR <victorgranier.pierre@gmail.com>
2023-01-27 16:24:35 +0100

    c'est quoi ça encore

---
+++ vitrine/components/Admin/style.js
@@ -0,0 +1,13 @@
+const style = {
+    container: {
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "1fr 1fr",
+        gap: "0px 0px",
+        gridTemplateAreas: `
+            "nav"
+            "."
+        `
+    }
+}
+export default style;
