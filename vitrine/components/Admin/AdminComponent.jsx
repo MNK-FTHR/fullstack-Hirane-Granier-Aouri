a1d03db189c0fae7a997760e8a0f55a56346674d
MNK-FTHR <victorgranier.pierre@gmail.com>
2023-01-29 15:27:47 +0100

    e

--- vitrine/components/Admin/AdminComponent.jsx
+++ vitrine/components/Admin/AdminComponent.jsx
@@ -4,15 +4,19 @@ const AdminComponent = (props) => {
 
     return(
         <div style={style.container}>
+            <h1 style={style.title}>GESTION BACK OFFICE</h1>
             <div style={style.nav}>
                 <div style={style.nav1}>
-                    <p></p>
+                    <p>tab1</p>
                 </div>
                 <div style={style.nav2}>
-
+                    <p>tab2</p>
                 </div>
             </div>
-            <div style={style.table}></div>
+            <div style={style.table}>
+                <div>table header</div>
+                <div>table body</div>
+            </div>
         </div>
     )
 }
--- vitrine/components/Admin/style.js
+++ vitrine/components/Admin/style.js
@@ -1,10 +1,12 @@
 const style = {
     container: {
+        backgroundColor: "#F5F5F5",
         display: "grid",
         gridTemplateColumns: "1fr",
-        gridTemplateRows: "1fr 1fr",
+        gridTemplateRows: "1fr 1fr 1fr",
         gap: "0px 0px",
         gridTemplateAreas: `
+            "title"
             "nav"
             "table"
         `
@@ -19,8 +21,20 @@ const style = {
         `,
         gridArea: "nav",
     },
-    nav1: { gridArea: "nav1"},
-    nav2: { gridArea: "nav2"},
-    table: { gridArea: "table" }
+    title: { 
+        gridArea: "title" 
+    },
+    nav1: { 
+
+        gridArea: "nav1"
+    },
+    nav2: { 
+
+        gridArea: "nav2"
+    },
+    table: { 
+
+        gridArea: "table"
+     }
 }
 export default style;
